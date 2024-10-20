68c382d9ea8688c984f919def77c889b1db04ba9
bskdany <54674556+bskdany@users.noreply.github.com>
2024-10-20 13:15:08 -0400

    Implemented acknowledgement from pad_server to client

--- control_client/src/pad.h
+++ control_client/src/pad.h
@@ -15,6 +15,7 @@ int pad_init(pad_t *pad, const char *ip, uint16_t port);
 int pad_connect(pad_t *pad);
 int pad_connect_forever(pad_t *pad);
 int pad_disconnect(pad_t *pad);
-ssize_t pad_send(pad_t *pad, const void *buf, size_t n);
+ssize_t pad_send(pad_t *pad, struct iovec *iov, ssize_t iovlen);
+ssize_t pad_recv(pad_t *pad, void *buf, ssize_t n);
 
 #endif // _PAD_H_
