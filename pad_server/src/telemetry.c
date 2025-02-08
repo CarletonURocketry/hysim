3f3f1ad07fba9fdd7c339cf4760ac20005dcef30
Tony Lin <99093620+justapotato213@users.noreply.github.com>
2025-02-08 14:07:59 -0500

    add extra mass sensor and make temp rise quicker

--- pad_server/src/telemetry.c
+++ pad_server/src/telemetry.c
@@ -165,7 +165,7 @@ static void random_data(telemetry_sock_t *telem) {
         pressure_i = 250 + pressure * 40;
         telemetry_publish_data(telem, TELEM_PRESSURE, 4, time, &pressure_i);
 
-        temperature = (temperature + 1) % 20000 + 20000;
+        temperature = (temperature + 78) % 20000 + 20000;
         int32_t temp_i = temperature - 1;
         telemetry_publish_data(telem, TELEM_TEMP, 1, time, &temp_i);
         temp_i = temperature + 1;
@@ -176,8 +176,10 @@ static void random_data(telemetry_sock_t *telem) {
         telemetry_publish_data(telem, TELEM_TEMP, 4, time, &temp_i);
 
         mass = (mass + 10) % 4000 + 3900;
-        uint32_t mass_i = mass + 2;
+        uint32_t mass_i = mass + 1000;
         telemetry_publish_data(telem, TELEM_MASS, 1, time, &mass_i);
+        mass_i = mass  -  1000;
+        telemetry_publish_data(telem, TELEM_MASS, 2, time, &mass_i);
 
         time = (time + 1) % 1000000;
         usleep(100000);
