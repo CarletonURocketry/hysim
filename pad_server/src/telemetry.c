47c2ebd758caaff37a6e57dfbde55771324f46e0
Matteo Golin <matteo.golin@gmail.com>
2025-05-07 15:55:50 -0400

    Only log sensor reading to console when running in NSH

--- pad_server/src/telemetry.c
+++ pad_server/src/telemetry.c
@@ -292,10 +292,12 @@ void *telemetry_run(void *arg) {
             }
 
             err = adc_read_value(&adc_devices[i]);
+#ifdef CONFIG_SYSTEM_NSH
             if (err < 0) {
                 fprintf(stderr, "Failed to read ADC value from id %d: %d\n", adc_devices[i].id, err);
                 continue;
             }
+#endif
 
             for (int j = 0; j < adc_devices[i].n_channels; j++) {
                 adc_channel_t channel = adc_devices[i].channels[j];
@@ -304,15 +306,19 @@ void *telemetry_run(void *arg) {
                 /* For each channel of data we find the corresponding information */
                 int32_t sensor_val = 0;
                 err = adc_sensor_val_conversion(&channel, adc_val, &sensor_val);
+#ifdef CONFIG_SYSTEM_NSH
                 if (err < 0) {
                     fprintf(stderr, "ADC %d pin %d: failed to convert value\n", i, channel.channel_num - 4);
                     continue;
                 }
                 fprintf(stderr, "ADC %d pin %d value %ld\n", i, channel.channel_num - 4, adc_val);
+#endif
 
                 telemetry_publish_data(&telem, channel.type, channel.sensor_id, time_ms, (void *)&sensor_val);
             }
+#ifdef CONFIG_SYSTEM_NSH
             printf("\n");
+#endif
         }
 
         if (sensor_mass.available) {
